3627ec68eeeb60f4314d077bd31b0190a4cab592
polettit <tommaso.poletti.external@yoox.com>
2018-07-23 15:49:10 +0200

    modified some events for react synthetic evts

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
 import tmripple from './src';
-import React, {Component} from 'react';
+import React from 'react';
 
-function _getDisplayName(WrappedComponent) {
-return WrappedComponent.displayName || WrappedComponent.name || "Component";
-}
-
-function withRipple(WrappedComponent) {
-  return class extends React.Component {
-    static displayName = `HOC(${getDisplayName(WrappedComponent)})`;
-
-    componentDidMount() {
-      // to improve
-      if (this.button && this.button.updater.isMounted(this.button)) {
-        const { color, eventName } = this.props.tmripple;
-        const buttonEl = ReactDOM.findDOMNode(this.button);
-
-        buttonEl.addEventListener("click", e =>          tmripple.ripple.call(this, e, color, eventName)
-        );
-      }
-    }
-
-    render() {
-      return <WrappedComponent ref={el => (this.button = el)} />;
-    }
+const withRipple = Wcomp => props => {
+    const { color, eventName } = props.tmripple;
+    return (
+      <Wcomp
+        onClick={e => {
+          props.onClick.call(this, e);
+          tmripple.ripple.call(this, e, color, eventName);
+        }}
+      />
+    );
   };
-}
-
-// function withRipple(WrappedComponent) {
-// return class extends Component {
-//     static displayName = `HOC(${_getDisplayName(WrappedComponent)})`;
-//     render() {
-//     const { color, eventName } = this.props.tmripple;
-//     return (
-//         <div
-//         onClick={e => {
-//             tmripple.ripple.call(this, e, color, eventName);
-//         }}
-//         >
-//         <WrappedComponent />
-//         </div>
-//     );
-//     }
-// };
-// }
 
 export default withRipple;
