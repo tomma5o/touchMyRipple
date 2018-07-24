0d15330d06097605c28084af916f92643df514b6
polettit <tommaso.poletti.external@yoox.com>
2018-07-24 12:22:53 +0200

    changed file positions and fixed npm/git-ignore files

--- webpack/libraryTask.js
+++ webpack/libraryTask.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
 
     entry: {
@@ -6,7 +8,7 @@ module.exports = {
     output: {
         filename: './dist/touchMyRipple.js',
         library: 'tmripple',
-        libraryTarget: 'umd',
+        libraryTarget: 'window',
     },
     module: {
         rules: [
@@ -22,4 +24,7 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new CompressionPlugin()
+    ]
 };
